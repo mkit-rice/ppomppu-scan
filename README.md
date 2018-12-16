9f1d4a5da27963cbd79f9450d4cee204d68b7ca5
mkit-rice <mkit.rice@gmail.com>
2018-12-16 13:37:32 +0900

    sqlite 연동

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ppomppu-scan
+
+### ./config.py 생성
+```python
+# gmail smtp
+SMTP_INFO = {
+  'email': 'your@email',
+  'passwd': 'password',
+  'toEmail': 'receive@email'
+}
+```
--- scan_db.py
+++ scan_db.py
@@ -4,54 +4,65 @@ import requests
 import sys
 import config
 import datetime
-
 import smtplib
+import sqlite3
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from bs4 import BeautifulSoup as bs
+from urllib import parse
+
+# db connect
+conn = sqlite3.connect('search.db')
+curs = conn.cursor()
+
+# Create table
+curs.execute('''CREATE TABLE IF NOT EXISTS history
+            (keyword TEXT, no INTEGER)''')
+conn.commit()
+
 
 SMTP_INFO = config.SMTP_INFO
-today = datetime.datetime.now().strftime("%Y-%m-%d")[2:10] #18-10-31
+today = datetime.datetime.now().strftime("%Y.%m.%d")[2:10] #18-10-31
 
 with requests.Session() as s:
   keyword = sys.argv[1]
+  keywordQuery = parse.urlencode(parse.parse_qs('keyword='+keyword), doseq=True)
+
   no = []
   msg = ''
   lastNo = 0
 
-  first_page = s.get('http://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu&search_type=sub_memo&keyword='+str(keyword))
+  first_page = s.get('http://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu&search_type=sub_memo&'+str(keywordQuery))
   html = first_page.text
   soup = bs(html, 'html.parser')
   items = soup.select(
-    '.list0 table a'
+    '.list0'
   )
 
-  # 첫글 번호 읽기
-  f = open("/Users/i2801/python/ppomppu-scan/last.txt", 'r')
-  while True:
-      line = f.readline()
-      if not line: break
-      lastNo = int(line)
-  f.close()
-
   for item in items:
-    title = item.get_text()
-    link = item.get('href')
+    a = item.find('table').findAll('a')[1]
+    title = a.get_text()
+    link = a.get('href')
     nowNo = link.split('no=')[1].split('&')[0]
 
-    if int(nowNo) == int(lastNo):
-      continue
-
-    if int(nowNo) > int(lastNo):
+    # db 조회
+    search = (keyword,nowNo,)
+    curs.execute('SELECT * FROM history WHERE keyword=? AND no=?',search)
+    if curs.fetchone():
+      sys.exit()
+    else:
       msg = msg + '<a href="http://www.ppomppu.co.kr/zboard/view.php?id=ppomppu&no='+nowNo+'" target="_blank">'+title+'</a><br/>'
       no.append(nowNo)
 
+
   # 첫글 번호 저장
   if len(no) > 0:
-    f = open("/Users/i2801/python/ppomppu-scan/last.txt", 'w')
-    f.write(no[0])
-    f.close()
+    values = [(keyword,no[0])]
+    curs.executemany('insert into history values(?,?)', values)
+    conn.commit()
+    # print('insert db',values)
 
+    # 메일 발송
     if msg != '':
       email = config.SMTP_INFO["email"]
       passwd = config.SMTP_INFO["passwd"]
